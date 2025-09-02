b4824dda9413ec3d4e97bd61a56e9098b463af6b
pmendesl <pedro.gabrie2909@gmail.com>
2025-09-01 21:36:22 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -1,1 +1,19 @@
+#ifndef LISTA_H
+#define LISTA_H
 
+#include "aluno.h"
+
+typedef struct {
+    Aluno *alunos;
+    int tamanho;
+    int capacidade;
+} Lista;
+
+void inicializarLista(Lista *lista, int capacidade);
+void inserirAluno(Lista *lista, Aluno aluno);
+void removerAluno(Lista *lista, int matricula);
+void imprimirLista(Lista *lista);
+int buscarAlunoPorRM(Lista l, int rm);
+float calcularMedia(Lista l);
+
+#endif
